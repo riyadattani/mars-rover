4bce4e8547ecf16bbfe1c1b709ba0399842ee406
Riya Dattani <riya_dattani@hotmail.com>
2019-07-01 12:25:58 +0100

    rover can turn left

--- lib/location.rb
+++ lib/location.rb
@@ -10,6 +10,13 @@ class Location
     'W' => 'N'
   }.freeze
 
+  LEFT = {
+    'N' => 'W',
+    'E' => 'N',
+    'S' => 'E',
+    'W' => 'S'
+  }.freeze
+
   def initialize(x, y, direction)
     @x = x
     @y = y
@@ -18,5 +25,9 @@ class Location
 
   def turn_right
     @direction = RIGHT[@direction]
+  end
+
+  def turn_left
+    @direction = LEFT[@direction]
   end
 end
--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -28,4 +28,30 @@ describe Location do
       expect(location.direction).to eq('N')
     end
   end
+
+  describe '#turn_left' do
+    it 'change the direction from north to west' do
+      location = Location.new(0, 0, 'N')
+      location.turn_left
+      expect(location.direction).to eq('W')
+    end
+
+    it 'change the direction from east to north' do
+      location = Location.new(0, 0, 'E')
+      location.turn_left
+      expect(location.direction).to eq('N')
+    end
+
+    it 'change the direction from south to east' do
+      location = Location.new(0, 0, 'S')
+      location.turn_left
+      expect(location.direction).to eq('E')
+    end
+
+    it 'change the direction from west to south' do
+      location = Location.new(0, 0, 'W')
+      location.turn_left
+      expect(location.direction).to eq('S')
+    end
+  end
 end
