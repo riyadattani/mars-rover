cd3414af622b8fc21c3462efa1fd8d7d190949a9
Riya Dattani <riya_dattani@hotmail.com>
2019-07-01 15:32:06 +0100

    interpret the string

--- lib/input.rb
+++ lib/input.rb
@@ -1,9 +1,35 @@
 class Input
+  attr_reader :instructions
+
   def initialize(string)
     @string = string
+    @instructions = []
   end
 
   def plateau_coordinates
-    @string.split(/\n/)[0]
+    split_by_line[0]
+  end
+
+  def create_instructions
+    i = 1
+    while i <= @string.count("\n")
+      @instructions << process_string(i)
+      i += 2
+    end
+  end
+
+  private
+  
+  def process_string(i)
+    {
+      x: split_by_line[i].split(' ')[0].to_i,
+      y: split_by_line[i].split(' ')[1].to_i,
+      direction: split_by_line[i].split(' ')[2],
+      route: split_by_line[i + 1]
+    }
+  end
+
+  def split_by_line
+    @string.split(/\n/)
   end
 end
--- spec/input_spec.rb
+++ spec/input_spec.rb
@@ -1,10 +1,33 @@
 require 'input'
 
 describe Input do
+  before(:each) do
+    @input = Input.new("5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM")
+  end
+
   describe '#plateau_coordinates' do
     it 'separates the plateau coordinates' do
-      input = Input.new("5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM")
-      expect(input.plateau_coordinates).to eq('5 5')
+      expect(@input.plateau_coordinates).to eq('5 5')
+    end
+  end
+
+  describe '#instructions' do
+    it 'identifies the instructions' do
+      @input.create_instructions
+      expect(@input.instructions).to eq([
+        {
+          x: 1,
+          y: 2,
+          direction: 'N',
+          route: 'LMLMLMLMM'
+        },
+        {
+          x: 3,
+          y: 3,
+          direction: 'E',
+          route: 'MMRMMRMRRM'
+        }
+        ])
     end
   end
 end
